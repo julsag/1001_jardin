59f65f268513db77b42ecd912df705cc329108f1
julsag <sagotjul@gmail.com>
2016-10-26 18:48:50 +0200

    new

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,3 +2,21 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
+$(document).ready(function () {         
+    $(function(){
+        var current_page_URL = location.href;
+
+        $( "a" ).each(function() {
+
+            if ($(this).attr("href") !== "#") {
+
+                var target_URL = $(this).prop("href");
+
+                    if (target_URL == current_page_URL) {
+                        $('nav a').parents('li, ul').removeClass('active');
+                        $(this).parent('li').addClass('active');
+
+                        return false;
+                    }
+            }
+        }); }); });
